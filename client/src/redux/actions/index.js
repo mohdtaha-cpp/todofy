6daa16ce11e9db2c614f65e94790fea8e7095df9
Mohd Taha <photonica.cool@gmail.com>
2024-03-10 00:32:16 +0530

    Updated the deployment link in frontend

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO, TOGGLE_TAB } from './type';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://todofy-production.up.railway.app';
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
         const res = await axios.post(`${API_URL}/todos`, { data });
 
-        dispatch({ type: ADDNEW_TODO , payload: res.data });
+        dispatch({ type: ADDNEW_TODO, payload: res.data });
     } catch (error) {
         console.log('Error while calling addNewTodo API ', error.message);
     }
@@ -18,7 +18,7 @@ export const getAllTodos = () => async (dispatch) => {
     try {
         const res = await axios.get(`${API_URL}/todos`);
 
-        dispatch({ type: GETALL_TODO , payload: res.data });
+        dispatch({ type: GETALL_TODO, payload: res.data });
     } catch (error) {
         console.log('Error while calling getAllTodos API ', error.message);
     }
@@ -28,7 +28,7 @@ export const toggleTodo = (id) => async (dispatch) => {
     try {
         const res = await axios.get(`${API_URL}/todos/${id}`);
 
-        dispatch({ type: TOGGLE_TODO , payload: res.data });
+        dispatch({ type: TOGGLE_TODO, payload: res.data });
     } catch (error) {
         console.log('Error while calling getAllTodos API ', error.message);
     }
@@ -38,7 +38,7 @@ export const updateTodo = (id, data) => async (dispatch) => {
     try {
         const res = await axios.put(`${API_URL}/todos/${id}`, { data });
 
-        dispatch({ type: UPDATE_TODO , payload: res.data });
+        dispatch({ type: UPDATE_TODO, payload: res.data });
     } catch (error) {
         console.log('Error while calling updateTodo API ', error.message);
     }
@@ -48,7 +48,7 @@ export const deleteTodo = (id) => async (dispatch) => {
     try {
         const res = await axios.delete(`${API_URL}/todos/${id}`);
 
-        dispatch({ type: DELETE_TODO , payload: res.data });
+        dispatch({ type: DELETE_TODO, payload: res.data });
     } catch (error) {
         console.log('Error while calling deleteTodo API ', error.message);
     }
